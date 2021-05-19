2259cfbfd03972bf045ab4c3136b87ade48b8dd2
BryantQue <neil.que@obf.ateneo.edu>
2021-05-19 15:16:46 +0800

    fixing issues on local save (2)

--- lib/main.dart
+++ lib/main.dart
@@ -18,42 +18,25 @@ class LoadingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
+      return Container(
       color: Color.fromRGBO(66, 133, 244, 1),
-<<<<<<< HEAD
-      child: Row(
-=======
       child: Column(
->>>>>>> 3f6c056639b48e470317dcf0588200380e09cf01
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          Column(
+          Row(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Container(
+                margin: const EdgeInsets.all(10.0),
                 child: CircleAvatar(
-                  radius: 36.0,
-                  child: Text('logo'),
+                  child: Text('Logo'),
                 ),
               ),
-              Divider(
-                height: 10.0,
-              ),
-              Container(
-                child: Text(
-                  'Developer Student Clubs',
-                  style: Theme.of(context).textTheme.headline5.copyWith(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-              ),
-              Container(
-                child: Text(
-                  'Loyola',
-                  style: Theme.of(context).textTheme.headline5.copyWith(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w400,
-                  ),
+              Text(
+               'Developer Student Clubs Loyola',
+                style: Theme.of(context).textTheme.headline6.copyWith(
+                  color: Colors.white,
+                  fontWeight: FontWeight.w400,
                 ),
               ),
             ],
