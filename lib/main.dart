6a4008915a22e0aca395651832d2d0b673a49def
BryantQue <neil.que@obf.ateneo.edu>
2021-06-11 22:53:04 +0800

    included dsc logo, changed background color

--- lib/main.dart
+++ lib/main.dart
@@ -18,40 +18,16 @@ class LoadingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Color.fromRGBO(66, 133, 244, 1),
+      color: Color.fromARGB(255, 49, 123, 253),
       child: Row(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
           Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
-              Container(
-                child: CircleAvatar(
-                  radius: 36.0,
-                  child: Text('Logo'),
-                ),
-              ),
-              Divider(
-                height: 10.0,
-              ),
-              Container(
-                child: Text(
-                  'Developer Student Clubs',
-                  style: Theme.of(context).textTheme.headline5.copyWith(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-              ),
-              Container(
-                child: Text(
-                  'Loyola',
-                  style: Theme.of(context).textTheme.headline5.copyWith(
-                    color: Colors.white,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-              ),
+              Image.asset(
+                'images/Loading_Screen_2.png'
+              )
             ],
           ),
         ],
