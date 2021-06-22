a1c9b764e984e4651ec5c78ab62b7884da24ca1b
BryantQue <neil.que@obf.ateneo.edu>
2021-06-22 17:31:25 +0800

    finished overview, whole screen is stateless; few changes from figma file

--- lib/main.dart
+++ lib/main.dart
@@ -9,105 +9,291 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Tracker',
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        primaryColor: Colors.white
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: Tracker(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class Tracker extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
     return Scaffold(
       appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+        actions: [
+          IconButton(icon: Icon(Icons.menu, size: 36.0,), onPressed: null)
+        ],
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
+      body: Container(
+        margin: EdgeInsets.only(top: 32.0, left: 16.0, right: 16.0),
         child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  "Overview",
+                  style: Theme.of(context).textTheme.headline4.copyWith(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black
+                  ),
+                ),
+              ],
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Divider(height: 16.0,),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Color.fromARGB(255, 49, 123, 253)
+                  ),
+                  height: 96.0,
+                  width: 180.0,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.event_available_outlined, 
+                              color: Colors.white, 
+                              size: 36.0,
+                            )
+                          ],
+                        ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Events",
+                                style: Theme.of(context).textTheme.subtitle1.copyWith(
+                                  color: Colors.white,
+                                ),
+                              ),
+                              Text(
+                                "Applied",
+                                style: Theme.of(context).textTheme.subtitle1.copyWith(
+                                  color: Colors.white
+                                ),
+                              ),
+                              Text(
+                                "0",
+                                style: Theme.of(context).textTheme.headline5.copyWith(
+                                  color: Colors.white
+                                ),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Color.fromARGB(255, 49, 123, 253)
+                  ),
+                  height: 96.0,
+                  width: 180.0,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.assignment_turned_in_outlined,
+                              color: Colors.white,
+                              size: 36.0,
+                            )
+                          ],
+                        ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Projects",
+                                style: Theme.of(context).textTheme.subtitle1.copyWith(
+                                  color: Colors.white
+                                ),
+                              ),
+                              Text(
+                                "Applied",
+                                style: Theme.of(context).textTheme.subtitle1.copyWith(
+                                  color: Colors.white
+                                ),
+                              ),
+                              Text(
+                                "0",
+                                style: Theme.of(context).textTheme.headline5.copyWith(
+                                  color: Colors.white
+                                ),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
             ),
+            Divider(height: 16.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceAround,
+              children: [
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Color.fromARGB(255, 49, 123, 253)
+                  ),
+                  height: 96.0,
+                  width: 180.0,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.lightbulb_outlined, 
+                              color: Colors.white, 
+                              size: 36.0,
+                            )
+                          ],
+                        ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Projects",
+                                style: Theme.of(context).textTheme.subtitle1.copyWith(
+                                  color: Colors.white,
+                                ),
+                              ),
+                              Text(
+                                "Saved",
+                                style: Theme.of(context).textTheme.subtitle1.copyWith(
+                                  color: Colors.white
+                                ),
+                              ),
+                              Text(
+                                "0",
+                                style: Theme.of(context).textTheme.headline5.copyWith(
+                                  color: Colors.white
+                                ),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+                Container(
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(15),
+                    color: Color.fromARGB(255, 49, 123, 253)
+                  ),
+                  height: 96.0,
+                  width: 180.0,
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                    children: [
+                      Column(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Icon(
+                              Icons.calendar_today_outlined,
+                              color: Colors.white,
+                              size: 36.0,
+                            )
+                          ],
+                        ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.start,
+                        children: [
+                          Column(
+                            mainAxisAlignment: MainAxisAlignment.center,
+                            children: [
+                              Text(
+                                "Jobs",
+                                style: Theme.of(context).textTheme.subtitle1.copyWith(
+                                  color: Colors.white
+                                ),
+                              ),
+                              Text(
+                                "Saved",
+                                style: Theme.of(context).textTheme.subtitle1.copyWith(
+                                  color: Colors.white
+                                ),
+                              ),
+                              Text(
+                                "0",
+                                style: Theme.of(context).textTheme.headline5.copyWith(
+                                  color: Colors.white
+                                ),
+                              )
+                            ],
+                          ),
+                        ],
+                      ),
+                    ],
+                  ),
+                ),
+              ],
+            ),
+            Divider(height: 16.0),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: [
+                Text(
+                  "Activity",
+                  style: Theme.of(context).textTheme.headline4.copyWith(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.black
+                  ),
+                ),
+              ],
+            )
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+      bottomNavigationBar: BottomNavigationBar(
+        currentIndex: 2,
+        fixedColor: Color.fromARGB(255, 49, 123, 253),
+        items: [
+          BottomNavigationBarItem(
+            label: "CALENDAR",
+            icon: Icon(Icons.calendar_today)
+          ),
+          BottomNavigationBarItem(
+            label: "HOME",
+            icon: Icon(Icons.home)
+          ),
+          BottomNavigationBarItem(
+            label: "TRACKER",
+            icon: Icon(Icons.mobile_friendly)
+          ),
+        ],
+        onTap: null,
+      ),
     );
   }
 }
